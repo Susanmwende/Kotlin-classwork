24fe0791ca323a5ce74cdae0f4f5cddfabd4d420
student <mwendezmasaku@gmail.com>
2024-02-16 20:09:47 +0300

    classwork

---
+++ src/Main.kt
@@ -0,0 +1,70 @@
+fun main() {
+    var num1 = 64.0
+    var num2 = 35
+    var result = num1 + num2
+    println(result)
+    var firstName = "Jack "
+    var lastName = "Black"
+    var fullname = firstName +  " " +lastName
+    println(fullname)
+     num1 = 3.4
+     num2 = 35
+   var difference = num1 - num2
+    println(difference)
+
+    num1 = 8
+    num2 = 5
+    result = num1 % num2
+    println(result)
+
+    var a = 8
+    var b  = 5
+    a += b
+    println(a)
+
+    a =8
+    b = 5
+    a -= b
+    println(a)
+
+    a = 8
+    b = 5
+    a *= b
+    println(a)
+
+    a = 8
+    a++
+    println(a)
+
+    a = 8
+    a--
+    println(a)
+
+    a = 8
+    ++a
+    println(a)
+
+     a = 8
+     b = 5
+    println(a>b)
+
+    a = 8
+    b = 5
+    println(a>=b)
+
+    a = 8
+    b = 5
+    println(8<5)
+
+    a = 5
+    b = 5
+    println(a==b)
+
+    a = 8
+    b = 5
+    println(a!=b)
+
+
+
+
+}
